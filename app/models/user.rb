bf57b3d9ace5bd4b44f172c62a813ad497675f0c
Sabina <sabina.vojic@habmann.info>
2023-10-06 11:10:10 +0200

    Adding an author

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,4 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false },
                       length: { maximum: 105 },
                       format: { with: VALID_EMAIL_REGEX }
-  has_secure_password
 end
