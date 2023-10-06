ea4307aeb4024316c65effd8e5cfe85328d75bfe
Sabina <sabina.vojic@habmann.info>
2023-10-06 09:05:45 +0200

    Drying code and partial templates

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy] #where is method used
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -15,18 +15,17 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params) #simply call method from private
     if @article.save
+      redirect_to @article
     end
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -35,9 +34,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
+  private #without end block (should always be to the bottom)
+
+  #method for getting and ID
+  def set_article
+  @article = Article.find(params[:id])
+  end
+
+  #method for params
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
