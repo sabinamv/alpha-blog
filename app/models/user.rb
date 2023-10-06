8478e365f75057ad225df522cddcfc69c57a7eeb
Sabina <sabina.vojic@habmann.info>
2023-10-06 13:16:44 +0200

    Signup func working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  has_many :articles
   has_many :articles, dependent: :destroy
   validates :username, presence: true,
                       uniqueness: { case_sensitive: false },
@@ -10,5 +9,5 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false },
                       length: { maximum: 105 },
                       format: { with: VALID_EMAIL_REGEX }
-#  has_secure_password
+  has_secure_password
 end
